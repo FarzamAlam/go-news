8c7f7ca25b7ac614baf5a4fdd2f245b9eec793ba
FarzamAlam <farzamcse@gmail.com>
2020-06-09 02:37:44 +0000

    Added error handler in case the api fails

--- handlers/newsHandler.go
+++ handlers/newsHandler.go
@@ -21,55 +21,6 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-// SearchHandler ...
-func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	searchKey := query.Get("q")
-	page := query.Get("page")
-	if page == "" {
-		page = "1"
-	}
-	log.Println("Search Query is : ", searchKey)
-	log.Println("Result Page is : ", page)
-
-	search := &Search{}
-	search.SearchKey = searchKey
-	next, err := strconv.Atoi(page)
-	if err != nil {
-		log.Println("Error while parsing page :", err)
-		http.Error(w, "Unexpected Server error", http.StatusInternalServerError)
-		return
-	}
-	search.NextPage = next
-	pageSize := 20
-	endPoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
-	resp, err := http.Get(endPoint)
-	if err != nil {
-		log.Println("Error calling the endPoint")
-		log.Println("endPoint : ", endPoint)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Println("Status code is != 200")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	err = json.NewDecoder(resp.Body).Decode(&search.Results)
-	if err != nil {
-		log.Println("Error while decoding the json body : ", err)
-	}
-	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
-	if ok := !search.IsLastPage(); ok {
-		search.NextPage++
-	}
-	err = tpl.Execute(w, search)
-	if err != nil {
-		log.Println("Error while tpl.Execute : ", err)
-	}
-}
-
 type Results struct {
 	Status       string    `json:"status"`
 	TotalResults int       `json:"totalResults"`
@@ -127,4 +78,64 @@ func (s *Search) CurrentPage() int {
 
 func (s *Search) PreviousPage() int {
 	return s.CurrentPage() - 1
+}
+
+type NewsAPIError struct {
+	Status  string `json:"status"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+// SearchHandler ...
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	searchKey := query.Get("q")
+	page := query.Get("page")
+	if page == "" {
+		page = "1"
+	}
+	log.Println("Search Query is : ", searchKey)
+	log.Println("Result Page is : ", page)
+
+	search := &Search{}
+	search.SearchKey = searchKey
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		log.Println("Error while parsing page :", err)
+		http.Error(w, "Unexpected Server error", http.StatusInternalServerError)
+		return
+	}
+	search.NextPage = next
+	pageSize := 20
+	endPoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	resp, err := http.Get(endPoint)
+	if err != nil {
+		log.Println("Error calling the endPoint")
+		log.Println("endPoint : ", endPoint)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		newsError := &NewsAPIError{}
+		err := json.NewDecoder(resp.Body).Decode(newsError)
+		if err != nil {
+			http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		}
+		http.Error(w, newsError.Message, http.StatusInternalServerError)
+		log.Println("Status code is != 200")
+		return
+	}
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		log.Println("Error while decoding the json body : ", err)
+	}
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	if ok := !search.IsLastPage(); ok {
+		search.NextPage++
+	}
+	err = tpl.Execute(w, search)
+	if err != nil {
+		log.Println("Error while tpl.Execute : ", err)
+	}
 }
